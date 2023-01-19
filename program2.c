b99fbad1840ccf1328dbe8067e1b283f80713f02
maheshbghule2000 <111970471+maheshbghule2000@users.noreply.github.com>
2023-01-19 15:06:09 +0530

    update

---
+++ program2.c
@@ -0,0 +1,17 @@
+// print 5 times “Baap”
+#include<stdio.h>
+void Display()
+{
+    int i = 0;
+
+   for(i = 1; i<= 5 ;i++)
+    {
+        printf("baap\n");
+    }
+}
+int main()
+{
+    Display();
+
+    return 0;
+}
---
+++ program3.c
@@ -0,0 +1,18 @@
+// 3) print 5 to 1 numbers.c
+#include<stdio.h>
+void Display()
+{
+    int i = 0;
+    i = 5;
+
+   while(i>0)
+    {
+        printf("%d\n",i);
+        i--;
+    }
+ }
+int main()
+{
+    Display();
+    return 0;
+}
